6b197c5d7f295983dc3527cd00a5932b8b432db9
Omar Gonzalez <omar.gonzalez@leafgroup.com>
2024-01-14 02:49:58 -0800

    chore: labeled harpoon keybindings

--- neovim/.config/nvim/lua/custom/plugins.lua
+++ neovim/.config/nvim/lua/custom/plugins.lua
@@ -129,11 +129,11 @@ local plugins = {
       })
       vim.keymap.set("n", "<leader>a", function () harpoon:list():append() end, { desc = "Add file to Harpoon" })
       vim.keymap.set("n", "<leader>H", function () harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Open Harpoon Window"})
-      vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-      vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
-      vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
-      vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
-      vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
+      vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Open Harpoon File 1" })
+      vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Open Harpoon File 2" })
+      vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Open Harpoon File 3" })
+      vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Open Harpoon File 4" })
+      vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end, { desc = "Open Harpoon File 5" })
 
       -- Telescope Setup
       local conf = require("telescope.config").values
