ea78e019d8cce99d2a40ea5b5a433ae6e2bc78ef
Omar Gonzalez <omar.gonzalez@leafgroup.com>
2024-01-14 02:42:54 -0800

    feat: added harpoon2 plugin and set up keybindings

--- neovim/.config/nvim/lua/custom/plugins.lua
+++ neovim/.config/nvim/lua/custom/plugins.lua
@@ -113,6 +113,50 @@ local plugins = {
     opts = {
       -- trouble configurations go here
     }
+  },
+  {
+    "ThePrimeagen/harpoon",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    branch = "harpoon2",
+    lazy = false,
+    config = function ()
+      local harpoon = require("harpoon")
+      harpoon:setup({
+        settings = {
+          save_on_toggle = true,
+          save_on_ui_close = true,
+        }
+      })
+      vim.keymap.set("n", "<leader>a", function () harpoon:list():append() end, { desc = "Add file to Harpoon" })
+      vim.keymap.set("n", "<leader>H", function () harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Open Harpoon Window"})
+      vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+      vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+      vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+      vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+      vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
+
+      -- Telescope Setup
+      local conf = require("telescope.config").values
+      local function toggle_telescope(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require("telescope.pickers").new({}, {
+          prompt_title = "Harpoon",
+          finder = require("telescope.finders").new_table({
+            results = file_paths
+          }),
+          previewer = conf.file_previewer({}),
+          sorter = conf.generic_sorter({}),
+        }):find()
+      end
+
+      vim.keymap.set("n", "<C-e>", function () toggle_telescope(harpoon:list()) end,
+        { desc = "Open Telescope Harpoon Window"}
+      )
+    end
   }
 }
 
