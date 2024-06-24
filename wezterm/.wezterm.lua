6f09ba73393ef554835f64d1ceb69e39bad64872
Omar Gonzalez <s9tpepper@apache.org>
2024-06-23 20:36:35 -0700

    feat: added wezterm dot files

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,50 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.color_scheme = "Catppuccin Mocha"
+config.colors = {
+	background = "black",
+}
+
+config.font = wezterm.font("BerkeleyMono Nerd Font Mono")
+config.font_size = 18.0
+
+config.freetype_load_target = "Normal"
+
+config.force_reverse_video_cursor = true
+
+-- Configure tabs
+-- callback fn (window, pane)
+-- Not sure what these right/left status areas are for
+config.hide_tab_bar_if_only_one_tab = true
+
+wezterm.on("update-right-status", function(window, _)
+	window:set_left_status("")
+	window:set_right_status("")
+end)
+
+config.use_fancy_tab_bar = false
+config.show_tabs_in_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+
+-- window background
+config.window_background_opacity = 0.92
+-- config.window_background_opacity = 0.86
+-- config.window_background_opacity = 1
+
+-- cursor settings
+config.cursor_blink_rate = 800
+config.cursor_blink_ease_in = "EaseIn"
+config.cursor_blink_ease_out = "EaseOut"
+
+-- window customizations, remove title bar
+config.window_decorations = "RESIZE"
+
+config.term = "wezterm"
+
+-- cursor
+config.default_cursor_style = "BlinkingBlock"
+config.cursor_blink_rate = 300
+
+return config
