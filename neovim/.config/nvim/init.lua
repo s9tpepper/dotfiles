afc26ca155412e13cfd12ca4ef6933a614a8356c
Omar Gonzalez <s9tpepper@apache.org>
2024-09-04 14:46:54 -0700

    feat: add codesnap
    fix: switch from eslint to eslint_d

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -452,15 +452,23 @@ require('lazy').setup({
     'folke/which-key.nvim',
     event = 'VeryLazy', -- Sets the loading event to 'VeryLazy'
     config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+      -- require('which-key').setup()
+      local wk = require 'which-key'
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+      wk.add {
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>c_', hidden = true },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>d_', hidden = true },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>r_', hidden = true },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>s_', hidden = true },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>w_', hidden = true },
+        { '<leader>x', group = '[X]Trouble' },
+        { '<leader>x_', hidden = true },
       }
     end,
   },
@@ -1023,7 +1031,8 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format lua code
         'js-debug-adapter',
-        'eslint-lsp',
+        -- 'eslint-lsp',
+        'eslint_d',
         'prettier',
         -- 'typescript-language-server',
         -- 'gopls',
--- neovim/.config/nvim/lua/custom/configs/lint.lua
+++ neovim/.config/nvim/lua/custom/configs/lint.lua
@@ -1,8 +1,8 @@
 require('lint').linters_by_ft = {
-  javascript = { 'eslint' },
-  javascriptreact = { 'eslint' },
-  typescript = { 'eslint' },
-  typescriptreact = { 'eslint' },
+  javascript = { 'eslint_d' },
+  javascriptreact = { 'eslint_d' },
+  typescript = { 'eslint_d' },
+  typescriptreact = { 'eslint_d' },
   -- json = {'eslint'}
 }
 
