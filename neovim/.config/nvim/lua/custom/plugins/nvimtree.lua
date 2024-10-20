917f7c526522c33ede02f425b4fd1833db72bad7
Omar Gonzalez <s9tpepper@apache.org>
2024-10-20 10:05:24 -0700

    fix: bat integration with fzf
    fix: now shows hidden files in nvim-tree by default

--- neovim/.config/nvim/lua/custom/plugins/nvimtree.lua
+++ neovim/.config/nvim/lua/custom/plugins/nvimtree.lua
@@ -5,7 +5,14 @@ return {
     'nvim-tree/nvim-web-devicons', -- optional
   },
   config = function()
-    require('nvim-tree').setup()
+    require('nvim-tree').setup {
+      git = {
+        ignore = false,
+      },
+      filters = {
+        dotfiles = false,
+      },
+    }
     vim.keymap.set('n', '<leader>t', '<Cmd>NvimTreeToggle<CR>')
   end,
 }
