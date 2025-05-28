d925f3a396ee3a2e71b2b0bf7c823818e2d374c8
s9tpepper <s9tpepper@apache.org>
2025-05-28 01:18:44 -0700

    feat: added scripts for stream layout for hyprland

---
+++ hyprland/.config/hypr/fix_stream_layout.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+source ./layout_functions.sh
+
+enable_floating "wezterm_tmux"
+resize "wezterm_tmux" 2779 2109
+move_to "wezterm_tmux" 12 39
+
+enable_floating "wezterm_sergeant"
+resize "wezterm_sergeant" 1023 842
+move_to "wezterm_sergeant" 2805 39
+
+enable_floating "wezterm_cam"
+resize "wezterm_cam" 1023 700
+move_to "wezterm_cam" 2805 895
+
+enable_floating "wezterm_keyboard"
+resize "wezterm_keyboard" 1023 539
+move_to "wezterm_keyboard" 2805 1609
