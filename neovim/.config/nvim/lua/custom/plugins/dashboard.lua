46172356f1189c262d9e06aebeb4860f94a7886e
s9tpepper <s9tpepper@apache.org>
2025-08-24 13:31:29 -0700

    fix: simplify dashboard

--- neovim/.config/nvim/lua/custom/plugins/dashboard.lua
+++ neovim/.config/nvim/lua/custom/plugins/dashboard.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require('dashboard').setup {
       theme = 'hyper',
-      disable_move = false,
+      disable_move = true,
       shortcut_type = 'letter',
       shuffle_letter = true,
       change_to_vcs_root = false,
@@ -15,17 +15,48 @@ return {
       },
       config = {
         header = {
-          '   ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗',
-          '   ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║',
-          '   ██╔██╗ ██║█████╗  ██║   ██║╚██╗ ██╔╝██║██╔████╔██║',
-          '   ██║╚██╗██║██╔══╝  ██║   ██║ ╚████╔╝ ██║██║╚██╔╝██║',
-          '   ██║ ╚████║███████╗╚██████╔╝  ╚██╔╝  ██║██║ ╚═╝ ██║',
-          '   ╚═╝  ╚═══╝╚══════╝ ╚═════╝    ╚═╝   ╚═╝╚═╝     ╚═╝',
+          '  ███████╗ █████╗ ████████╗██████╗ ███████╗██████╗ ██████╗ ███████╗██████╗ ',
+          '  ██╔════╝██╔══██╗╚══██╔══╝██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔════╝██╔══██╗',
+          '  ███████╗╚██████║   ██║   ██████╔╝█████╗  ██████╔╝██████╔╝█████╗  ██████╔╝',
+          '  ╚════██║ ╚═══██║   ██║   ██╔═══╝ ██╔══╝  ██╔═══╝ ██╔═══╝ ██╔══╝  ██╔══██╗',
+          '  ███████║ █████╔╝   ██║   ██║     ███████╗██║     ██║     ███████╗██║  ██║',
+          '  ╚══════╝ ╚════╝    ╚═╝   ╚═╝     ╚══════╝╚═╝     ╚═╝     ╚══════╝╚═╝  ╚═╝',
         },
         week_header = {
           enable = false,
         },
+
+        shortcut = {
+          { desc = '[  Github]', group = 'DashboardShortCut' },
+          { desc = '[  s9tpepper]', group = 'DashboardShortCut' },
+        },
+
+        mru = { enable = false },
+        project = { enable = false },
         footer = {},
+
+        center = {
+          {
+            icon = ' ',
+            icon_hl = 'Title',
+            desc = 'Find File           ',
+            desc_hl = 'String',
+            key = 'b',
+            keymap = 'SPC f f',
+            key_hl = 'Number',
+            key_format = ' %s', -- remove default surrounding `[]`
+            action = 'lua print(2)',
+          },
+          {
+            icon = ' ',
+            desc = 'Find Dotfiles',
+            key = 'f',
+            keymap = 'SPC f d',
+            key_format = ' %s', -- remove default surrounding `[]`
+            action = 'lua print(3)',
+          },
+        },
+        vertical_center = true, -- Center the Dashboard on the vertical (from top to bottom)
       },
     }
   end,
