900b1ba469096f8d21d24bbbd602d5c3e39f84bb
s9tpepper <s9tpepper@apache.org>
2025-08-24 13:33:47 -0700

    fix: update smear-cursor configs

--- neovim/.config/nvim/lua/custom/plugins/smear.lua
+++ neovim/.config/nvim/lua/custom/plugins/smear.lua
@@ -1,22 +1,41 @@
 return {
   'sphamba/smear-cursor.nvim',
-  opts = {
-    -- Smear cursor when switching buffers or windows.
-    smear_between_buffers = true,
+  opts = function()
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      callback = function()
+        vim.cmd 'SmearCursorToggle'
+      end,
+    })
+    vim.api.nvim_create_autocmd('BufWritePost', {
+      callback = function()
+        -- vim.cmd 'SmearCursorToggle'
 
-    -- Smear cursor when moving within line or to neighbor lines.
-    -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
-    smear_between_neighbor_lines = true,
+        vim.defer_fn(function()
+          vim.cmd 'SmearCursorToggle'
+        end, 5) -- delay 50ms
+      end,
+    })
 
-    -- Draw the smear in buffer space instead of screen space when scrolling
-    scroll_buffer_space = true,
+    return {
+      -- Smear cursor when switching buffers or windows. default: true
+      smear_between_buffers = false,
 
-    -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
-    -- Smears will blend better on all backgrounds.
-    legacy_computing_symbols_support = false,
+      -- Smear cursor when moving within line or to neighbor lines.
+      -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
+      smear_between_neighbor_lines = true,
 
-    -- Smear cursor in insert mode.
-    -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
-    smear_insert_mode = true,
-  },
+      -- Draw the smear in buffer space instead of screen space when scrolling
+      scroll_buffer_space = true,
+
+      -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+      -- Smears will blend better on all backgrounds. default: false
+      legacy_computing_symbols_support = true,
+
+      -- Smear cursor in insert mode.
+      -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
+      smear_insert_mode = true,
+
+      smear_to_cmd = false, -- default: true
+    }
+  end,
 }
