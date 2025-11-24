ed45b1e45052c2872e56f7421c556f9d2dbf9ecd
Omar Gonzalez <s9tpepper@apache.org>
2025-11-23 20:46:37 -0800

    feat: add install.sh file to install dotfile requirements like themes

---
+++ install.sh
@@ -0,0 +1,11 @@
+# Install bat catppuccin
+mkdir -p "$(bat --config-dir)/themes"
+
+wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Latte.tmTheme
+wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Frappe.tmTheme
+wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Macchiato.tmTheme
+wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
+
+bat cache --build
+
+wget -P "$(bat --config-dir)/delta/themes" https://raw.githubusercontent.com/catppuccin/delta/refs/heads/main/catppuccin.gitconfig
